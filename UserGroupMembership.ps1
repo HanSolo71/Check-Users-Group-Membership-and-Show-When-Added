fd80f7e6f9be57ba54bb7f6486f6ce6696834163
HanSolo71 <HanSolo552@gmail.com>
2018-10-29 13:22:28 -0400

    Add files via upload

---
+++ UserGroupMembership.ps1
@@ -0,0 +1,19 @@
+﻿#Display What Groups a User is Part of and When They Were Added
+
+Import-Module ActiveDirectory
+
+#Username you would like to search
+$username = "username" 
+$userobj = Get-ADUser $username 
+
+#Active Directory Server To Connect To
+$ActiveDirectoryServer = "domaincontroller.test.com"
+
+Get-ADUser $userobj.DistinguishedName -Properties memberOf | 
+Select-Object -ExpandProperty memberOf | 
+ForEach-Object { 
+Get-ADReplicationAttributeMetadata $_ -Server $ActiveDirectoryServer -ShowAllLinkedValues | 
+Where-Object {$_.AttributeName -eq 'member' -and 
+$_.AttributeValue -eq $userobj.DistinguishedName} | 
+Select-Object FirstOriginatingCreateTime, Object, AttributeValue 
+} | Sort-Object FirstOriginatingCreateTime -Descending | Out-GridView
